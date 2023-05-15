e2ca3999a87c421f369d3c448e484ee8084e52cb
Fwoglee <132837574+Fwoglee@users.noreply.github.com>
2023-05-15 18:40:33 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function Index() {
     <>
       <div className="py-20 mb-30 max-w-3xl mx-auto">
         <div className="flex w-full justify-center">
-      <img width="500" src="https://wiki.hypixel.net/images/2/2a/Minecraft_items_red_mushroom.png" />
+      <img width="500" src="" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           CBT | Cookie BOT

2a5d8242e2857ab1fe962c9ccd1a58746c15f6b4
Fwoglee <132837574+Fwoglee@users.noreply.github.com>
2023-05-15 19:13:10 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -1,5 +1,5 @@
 
-export default function Partners() {
+export default function Partner() {
 
     return (
         <>
@@ -9,21 +9,21 @@ export default function Partners() {
       </div>
                 <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
                     <i className="fal fa-stars text-amber-400 mr-2" />
-                    Partners
+                    Partner
                 </h1>
             </div>
 
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/6XGqdgE">
+            <a href="https://discord.gg/mmGQEvBmR2">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/68ef946cf7cc39e4063eed4d446f6dda.webp" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Kod Paylaş</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                        Share Your Codes, Can You Keep Your Projects Active Forever. World's Largest Website with Discord Interface!
+                                        Kodlarınızı Paylaşın, Projelerinizi Sonsuza Kadar Aktif Tutabilir misiniz? Discord Arayüzüne Sahip Dünyanın En Büyük Web Sitesi!
                                     </p>
                                 </div>     
                             </div>
@@ -32,14 +32,14 @@ export default function Partners() {
                     </div>
 
                     <div className="flex flex-col justify-center text-white rounded">
-                        <a href="https://discord.gg/Fy4WmSThRZ">
+                        <a href="https://discord.gg/mmGQEvBmR2">
                             <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/924312493537509407/721de16575a9048c9c6980613ebb99f0.webp" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">The Developers</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Geliştiriciler</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                    You can get help on software and similar issues or you can spend time chatting. How about being a cool developer?
+                                    Yazılım ve benzeri konularda yardım alabilir veya sohbet ederek vakit geçirebilirsiniz. Harika bir geliştirici olmaya ne dersiniz?
                                     </p>
                                 </div>     
                             </div>
